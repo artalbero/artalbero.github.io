e516e247cadac6700ce7ea4148cca30fc503a8ce
artalbero <aalbero@iessanvicente.com>
2023-10-20 20:25:15 +0200

    limpieza

--- hdocs/content_1.md
+++
@@ -1,7 +0,0 @@
-## Slide 1
-This is my first slide
----
-## Slide 2
-This is my second slide
----
-## Title 1
--- hdocs/pruebaPHP.php
+++
@@ -1,6 +0,0 @@
-<?php
-$title = "Mi Prueba con php";
-include 'header_reveal.php';
-include 'content_1.md';
-include 'footer_reveal.php';
-?>
